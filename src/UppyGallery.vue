27fe521b5151d18237baf508d3a8088357ac9529
chiragmistry <chiragmistry@twowords.io>
2019-10-07 18:26:43 +0530

    -Initial realese v0.1.0

---
+++ src/UppyGallery.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            Add Item
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'uppy-gallery'
+    }
+</script>
